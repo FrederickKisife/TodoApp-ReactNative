969fa105f39c5415f32e4b65da6eefa455b4d034
FrederickKisife <kilafrederick@gmail.com>
2024-09-23 04:18:17 -0700

    delete and trash icon unctionality

--- App.js
+++ App.js
@@ -64,10 +64,12 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
   },
-  content:{
-    padding:40,
+  content: {
+    padding: 40,
+    flex: 1,
   },
-  list:{
+  list: {
     marginTop: 20,
-  }
+    flex: 1,
+  },
 });
